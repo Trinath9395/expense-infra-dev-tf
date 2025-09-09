a4c5e61fbd42046be4c6b744c2d8570a106dd049
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-09 10:51:19 +0530

    00-vpc

---
+++ 00-vpc/vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  #source = "../terraformm-aws-vpc"
+  source = "git::https://github.com/Trinath9395/terraformm-aws-vpc.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  vpc_cidr = var.vpc_cidr
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+  is_peering_required = true 
+}
