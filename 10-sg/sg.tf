f262feb4e40f941d0f64b1ee0c9782b84cedc395
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-09 16:41:48 +0530

    50-app-alb

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -50,9 +50,18 @@ module "app_alb_sg" {
 
 resource "aws_security_group_rule" "app_alb_bastion" {
   type = "ingress"
-  from_port = "80"
-  to_port = "80"
+  from_port = 80
+  to_port = 80
   protocol = "tcp"
-  source_security_group_id = [module.bastion_sg.sg_id]
+  source_security_group_id = module.bastion_sg.sg_id
   security_group_id = module.app_alb_sg.sg_id
+}
+
+resource "aws_security_group_rule" "bastion_public" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id 
 }
