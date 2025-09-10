e4804b551cd6514357a9dd34166b0dc5af08217d
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-10 12:24:38 +0530

    vpc-rds

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -11,3 +11,15 @@ module "vpc" {
   database_subnet_cidr = var.database_subnet_cidr
   is_peering_required = true 
 }
+
+resource "aws_db_subnet_group" "expense" {
+  name = "${var.project_name}-${var.environment}"
+  subnet_ids = module.vpc.database_subnet_ids
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}"
+    }
+  )
+}
