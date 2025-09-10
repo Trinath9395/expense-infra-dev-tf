55fee8fa6666e34018b52c432455502d907b46b3
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-10 11:13:56 +0530

    sg

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -14,8 +14,8 @@ module "backend_sg" {
   common_tags = var.common_tags
   sg_description = "backend instances"
   sg_name = "backend"
-  project_name = "expense"
-  environment = "dev"
+  project_name = var.project_name
+  environment = var.environment
 }
 
 module "frontend_sg" {
@@ -24,8 +24,8 @@ module "frontend_sg" {
   common_tags = var.common_tags
   sg_description = "frontend instances"
   sg_name = "frontend"
-  project_name = "expense"
-  environment = "dev"
+  project_name = var.project_name
+  environment = var.environment
 }
 
 module "bastion_sg" {
@@ -34,8 +34,18 @@ module "bastion_sg" {
   common_tags = var.common_tags
   sg_description = "bastion instances"
   sg_name = "bastion"
-  project_name = "expense"
-  environment = "dev"
+  project_name = var.project_name
+  environment = var.environment
+}
+
+module "vpn_sg" {
+  source = "git::https://github.com/Trinath9395/terraform-aws-sgroup.git?ref=main"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_description = "VPN access for dev teams"
+  sg_name = "vpn"
+  project_name = var.project_name
+  environment = var.environment
 }
 
 module "app_alb_sg" {
@@ -44,8 +54,8 @@ module "app_alb_sg" {
   common_tags = var.common_tags
   sg_description = "Created for backend ALB instances"
   sg_name = "app-alb"
-  project_name = "expense"
-  environment = "dev"
+  project_name = var.project_name
+  environment = var.environment
 }
 
 resource "aws_security_group_rule" "app_alb_bastion" {
@@ -66,3 +76,47 @@ resource "aws_security_group_rule" "bastion_public" {
   security_group_id = module.bastion_sg.sg_id 
 }
 
+resource "aws_security_group_rule" "vpn_ssh" {
+  type = "ingress"
+  from_port = 22 
+  to_port = 22 
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443 
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type = "ingress"
+  from_port = 943
+  to_port = 943
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type = "ingress"
+  from_port = 1194
+  to_port = 1194
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id 
+  security_group_id = module.app_alb_sg.sg_id 
+}
