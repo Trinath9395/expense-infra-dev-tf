2d05b94964cecea6bd8b27c9ca2c803f5af3145b
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-11 11:33:57 +0530

    ansible-tf

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -7,4 +7,4 @@ sudo dnf install ansible -y
 
 #pull
 
-ansible-pull -i localhost -U https://github.com/Trinath9395/ansible-expense-roles.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+ansible-pull -i localhost -U https://github.com/Trinath9395/ansible-expense-roles.tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
