05070f104eada7d0250584a066ec432d604b6e02
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-12 07:34:03 +0530

    sg

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -162,7 +162,7 @@ resource "aws_security_group_rule" "backend_app_alb" {
   from_port = 8080
   to_port = 8080
   protocol = "tcp"
-  source_security_group_id = module.app_alb.sg_id 
+  source_security_group_id = module.app_alb_sg.sg_id 
   security_group_id = module.backend_sg.sg_id 
 }
 
