5b909e6b60f0c8fa6b41a26312ad11f471aa5792
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-12 20:01:37 +0530

    vpc

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -99,7 +99,7 @@ resource "aws_autoscaling_group" "backend" {
   name                      = local.backend_name
   max_size                  = 5
   min_size                  = 2
-  health_check_grace_period = 60
+  health_check_grace_period = 180
   health_check_type         = "ELB"
   desired_capacity          = 2
   target_group_arns = [aws_lb_target_group.backend.arn]
